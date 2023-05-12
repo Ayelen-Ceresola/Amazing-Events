dea1fad0c5fa7c74fd3278a795c1ba364394ab01
Ayelen-Ceresola <ayeceresola@hotmail.com>
2023-05-12 00:38:21 -0300

    repo Task3

---
+++ assets/archivosJs/details.js
@@ -0,0 +1,30 @@
+let containerDetail = document.getElementById(`containerDetail`)
+let info = data.events
+
+let parametros = new URLSearchParams (location.search)
+let capturaId = parametros.get(`_id`)
+let busquedaId = info.find (item => item._id == capturaId)
+
+containerDetail.innerHTML = `<div class="card mb-3" style="max-width: 540px;">
+<div class="row g-0">
+  <div>
+    <img class="col-12" src=${busquedaId.image} alt="${busquedaId.name}">
+  </div>
+  <div>
+    <div class="card-body">
+      <h5 class="card-title text-center">${busquedaId.name}</h5>
+      <div class="py-2">
+            <ul>
+                <li class="list">Date: ${busquedaId.date}</li>
+                <li class="list">Description: ${busquedaId.description}</li>
+                <li class="list">Category: ${busquedaId.category}</li>
+                <li class="list">Place: ${busquedaId.place}</li>
+                <li class="list">Capacity: ${busquedaId.capacity}</li>
+                <li class="list">${busquedaId.assistance ? "Assistance" : "Estimate"}: ${busquedaId.assistance ? busquedaId.assistance :busquedaId.estimate}</li>
+                <li class="list">Price: $${busquedaId.price}</li>
+            </ul>
+        </div>
+    </div>
+  </div>
+</div>
+</div>`
--- assets/archivosJs/pastEvents.js
+++ assets/archivosJs/pastEvents.js
@@ -1,33 +1,111 @@
 let containerCardPast = document.getElementById (`containerPastE`)   
 
-function creatCard (events) {
-    return `<div class="card d-flex flex-column mb-3 justify-content-center align-items-center m-2" style="width: 18rem;">
-            <img src=${events.image} class="card-img-top p-2" alt="Feria">
-            <div class="card-body d-flex flex-column align-items-center">
-            <h5 class="card-title">${events.name}</h5>
-            <p class="card-text text-center"> ${events.description}</p>
-            <h6 class="card-text">price: ${events.price}</h6>
-            <a href="./assets/pages/Details.html" class="btn btn-primary">See More</a>
-            </div>
-    </div>`
+// function creatCard (events) {
+//     return `<div class="card d-flex flex-column mb-3 justify-content-center align-items-center m-2" style="width: 18rem;">
+//             <img src=${events.image} class="card-img-top p-2" alt="Feria">
+//             <div class="card-body d-flex flex-column align-items-center">
+//             <h5 class="card-title">${events.name}</h5>
+//             <p class="card-text text-center"> ${events.description}</p>
+//             <h6 class="card-text">price: ${events.price}</h6>
+//             <a href="./assets/pages/Details.html" class="btn btn-primary">See More</a>
+//             </div>
+//     </div>`
     
-}
+// }
 
 
-function listaCrad (lista, donde){
-    let template = ``
-    for (let elemento of lista) {
-        template += creatCard (elemento)
+// function listaCrad (lista, donde){
+//     let template = ``
+//     for (let elemento of lista) {
+//         template += creatCard (elemento)
         
-    }
-    donde.innerHTML = template
-}
+//     }
+//     donde.innerHTML = template
+// }
 
-function filtro (eventos){
+ function filtro (eventos){
     return eventos.date < data.currentDate
 
+ }
+ let eventosFiltrados = data.events.filter((filtro))
+
+
+// listaCrad (eventosFiltrados, containerCardPast )
+
+let containerCheckbox = document.getElementById (`contenedor-checkbox`)
+let info = data.events
+
+//creacion del array + filtro
+const category = info.map (box => box.category)
+const setInfo = new Set (category)
+const arrayCategory = Array.from (setInfo)
+
+//creacion template de checkbox por categoria
+const funcionReduce = (acumulador,elementoActual, indice, array) =>{
+    return acumulador += `<div class="form-check">
+                            <input class="form-check-input" type="checkbox" id="${elementoActual} - ${indice}" value="${elementoActual}">
+                            <label class="form-check-label" for="${elementoActual} - ${indice}"> ${elementoActual}</label>
+                        </div>`
 }
-let eventosFiltrados = data.events.filter((filtro))
+const templateCategory = arrayCategory.reduce (funcionReduce , ``)
+containerCheckbox.innerHTML = templateCategory
 
+//creacion card
+function crearTemplateEventos (lista){
+    return lista.reduce ((acumulado, elementoA ) => {
+        return acumulado +=  `<div class="card d-flex flex-column mb-3 justify-content-center align-items-center m-2" style="width: 18rem;">
+                                    <img src=${elementoA.image} class="card-img-top p-2" alt="Feria">
+                                    <div class="card-body d-flex flex-column align-items-center">
+                                        <h5 class="card-title">${elementoA.name}</h5>
+                                        <p class="card-text text-center"> ${elementoA.description}</p>
+                                        <h6 class="card-text">price: ${elementoA.price}</h6>
+                                        <a href="../pages/Details.html?_id=${elementoA._id}" class="btn btn-primary">See More</a>
+                                    </div>
+                                </div>` 
+    }, ``)
+}
 
-listaCrad (eventosFiltrados, containerCardPast )
+containerCardPast.innerHTML = crearTemplateEventos (eventosFiltrados)
+
+// filtrar por check
+containerCheckbox.addEventListener  (`change`, ( ) => {
+    dobleFiltro ()
+    // const checkboxChecked = Array.from (document.querySelectorAll (`input[type="checkbox"]:checked`)).map ( check => check.value)
+    // console.log(checkboxChecked)
+    // const eventosFiltrados = filtrarEventos (info, checkboxChecked)
+    // containerCardIndex.innerHTML = crearTemplateEventos (eventosFiltrados)
+
+    
+})
+
+function filtrarEventos (datos, category){
+    if (category.length == 0){
+        return datos
+    }
+    return datos.filter ( datos => category.includes (datos.category))
+
+}
+
+//searche
+const inputBusqueda = document.getElementById (`busqueda`)
+
+inputBusqueda.addEventListener (`input`, () => {
+    dobleFiltro ()
+    
+    // const filtrarPorBusqueda = filtrarPorNombre( info, inputBusqueda.value)
+    // if (containerCheckbox.checked){
+    //     const filtrarPorCheck = filtrarEventos (filtrarPorBusqueda)
+    // }
+    
+})
+function filtrarPorNombre (datos, busqueda){
+    return datos.filter( item => item.name.toLowerCase().includes(busqueda.toLowerCase()))
+   
+}
+function dobleFiltro (){
+    const checkboxChecked = Array.from (document.querySelectorAll (`input[type="checkbox"]:checked`)).map ( check => check.value)
+    let filtrarPorBusqueda = filtrarPorNombre (eventosFiltrados, inputBusqueda.value )
+    let filtrarCheck = filtrarEventos (filtrarPorBusqueda, checkboxChecked)
+    containerCardPast.innerHTML = crearTemplateEventos (filtrarCheck)
+
+}
